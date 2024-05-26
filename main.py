1610e49a74fd38f3cdc3c6b4829d33d534370fa7
arnab28122000 <arnab.bhattacharya28122000@gmail.com>
2024-05-27 00:10:49 +0530

    added another cors origin

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
-    "https://aa-ui.vercel.app"
+    "https://aa-ui.vercel.app",
+    "https://setu-ui.dashtics.com"
 ]
 
 app.add_middleware(
