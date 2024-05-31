715f11c7bc49fd6fb4ee6c3ff2254ea155018f5c
arnab28122000 <arnab.bhattacharya28122000@gmail.com>
2024-05-31 12:30:44 +0530

    removed pass

--- aa_backend_service/db/__init__.py
+++ aa_backend_service/db/__init__.py
@@ -2,11 +2,9 @@ import os
 from sqlmodel import create_engine, Session
 
 db_username = os.environ['dashtics_username']
-# db_password = os.environ['dashtics_password']
+db_password = os.environ['dashtics_password']
 db_endpoint = os.environ['dashtics_endpoint']
 db_name = os.environ['dashtics_db_name']
-
-db_password = 'SyW3e9SvRKt4m>Ze!Wlr+%U~[7Sk'
 
 DATABASE_URL = f"postgresql://{db_username}:{db_password}@{db_endpoint}/{db_name}"
 
